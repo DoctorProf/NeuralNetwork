e4180a90d84c001f07b9d1180234cf52905f62b8
Dmitrii Osipov <dimaosipov907@gmail.com>
2023-11-23 21:07:44 +0300

    xor easy

--- NN.cpp
+++ NN.cpp
@@ -7,7 +7,7 @@ class Neuron
 {
 private:
 	std::vector<double> weights;
-	double bias;
+	bool toBias;
 	double value;
 	double error;
 	double gradient;
@@ -16,30 +16,16 @@ private:
 	{
 		for (size_t i = 0; i < weights.size(); i++) 
 		{
-			weights[i] = (rand() % 100) / 100.0;
-			//weights = 1;
+			weights[i] = (rand() % 100) / 50.0 - 1;
 		}
 	}
-	void initializeBias()
-	{
-		//bias = (rand() % 100) / 100.0;
-		bias = 0;
-	}
 public:
-	Neuron(int countWeight = 0, double value = 0) 
+	Neuron(int countWeight = 0, double value = 0, bool toBias = false) 
 	{
 		this->value = value;
 		this->weights.resize(countWeight);
+		this->toBias = toBias;
 		initializeWeights();
-		initializeBias();
-	}
-	void setBias(double bias) 
-	{
-		this->bias = bias;
-	}
-	double getBias()
-	{
-		return this->bias;
 	}
 	void setValue(double value)
 	{
@@ -73,6 +59,10 @@ public:
 	{
 		return this->weights;
 	}
+	bool getBias()
+	{
+		return this->toBias;
+	}
 };
 class NeuralNetwork 
 {
@@ -101,8 +91,16 @@ public:
 		this->hideNeurons.resize(countHideNeuron.size());
 		for (int i = 0; i < countInputNeuron; i++) 
 		{
-			inputNeurons.push_back(Neuron(countHideNeuron[0]));
+			if (countHideNeuron.size() == 0) 
+			{
+				inputNeurons.push_back(Neuron(countoutputNeuron));
+			}
+			else 
+			{
+				inputNeurons.push_back(Neuron(countHideNeuron[0] + 1));
+			}
 		}
+		if (useBias) inputNeurons.push_back(Neuron(countHideNeuron[0] + 1, 1, true));
 		for (int i = 0; i < countHideNeuron.size(); i++)
 		{
 			for (int j = 0; j < countHideNeuron[i]; j++)
@@ -113,7 +111,18 @@ public:
 				}
 				else 
 				{
-					hideNeurons[i].push_back(Neuron(countHideNeuron[i + 1]));
+					hideNeurons[i].push_back(Neuron(countHideNeuron[i + 1] + 1));
+				}
+			}
+			if (useBias) 
+			{
+				if (i == countHideNeuron.size() - 1)
+				{
+					hideNeurons[i].push_back(Neuron(countoutputNeuron, 1, true));
+				}
+				else
+				{
+					hideNeurons[i].push_back(Neuron(countHideNeuron[i + 1] + 1, 1, true));
 				}
 			}
 		}
@@ -124,11 +133,12 @@ public:
 	}
 	double getResult()
 	{
+		//std::cout << hideNeurons[0][0].getValue() << inputNeurons[0].getValue();
 		return outputNeurons[0].getValue();
 	}
 	void forwardPropagation(std::vector<double> inputs)
 	{
-		for (int i = 0; i < inputNeurons.size(); i++) 
+		for (int i = 0; i < inputNeurons.size() - 1; i++) 
 		{
 			inputNeurons[i].setValue(inputs[i]);
 		}
@@ -151,8 +161,15 @@ public:
 						sum += hideNeurons[i - 1][k].getValue() * hideNeurons[i - 1][k].getWeights()[j];
 					}
 				}
-				sum += hideNeurons[i][j].getBias();
-				hideNeurons[i][j].setValue(activate(sum));
+				if (hideNeurons[i][j].getBias())
+				{
+					hideNeurons[i][j].setValue(1);
+				}
+				else 
+				{
+					hideNeurons[i][j].setValue(activate(sum));
+				}
+				
 			}
 		}
 		for (int i = 0; i < outputNeurons.size(); i++)
@@ -161,8 +178,7 @@ public:
 			for (int j = 0; j < hideNeurons[hideNeurons.size() - 1].size(); j++)
 			{
 				sum += hideNeurons[hideNeurons.size() - 1][j].getValue() * hideNeurons[hideNeurons.size() - 1][j].getWeights()[i];
-			}
-			sum += outputNeurons[i].getBias();
+			};
 			outputNeurons[i].setValue(activate(sum));
 		}
 	}
@@ -177,31 +193,7 @@ public:
 		{
 			outputNeurons[i].setGradient(outputNeurons[i].getError() * derivative(outputNeurons[i].getValue()));
 		}
-		for (int i = 0; i < hideNeurons.size(); i++)
-		{
-			for (int j = 0; j < hideNeurons[i].size(); j++)
-			{
-				if (i == hideNeurons.size() - 1)
-				{
-					for (int k = 0; k < outputNeurons.size(); k++)
-					{
-						hideNeurons[i][j].setWeights(k, hideNeurons[i][j].getWeights()[k] + learningRate * outputNeurons[k].getGradient() * hideNeurons[i][j].getValue());
-					}
-				}
-				else
-				{
-					for (int k = 0; k < hideNeurons[i + 1].size(); k++)
-					{
-						hideNeurons[i][j].setWeights(k, hideNeurons[i][j].getWeights()[k] + learningRate * hideNeurons[i + 1][k].getGradient() * hideNeurons[i][j].getValue());
-					}
-				}
-			}
-		}
-		for (int k = 0; k < outputNeurons.size(); k++)
-		{
-			outputNeurons[k].setBias(outputNeurons[k].getBias() + learningRate * outputNeurons[k].getGradient());
-		}
-		for (int i = 0; i < hideNeurons.size(); i++)
+		for (int i = hideNeurons.size() - 1; i > -1; i--)
 		{
 			for (int j = 0; j < hideNeurons[i].size(); j++)
 			{
@@ -223,11 +215,32 @@ public:
 				hideNeurons[i][j].setError(sum);
 			}
 		}
-		for (int i = 0; i < hideNeurons.size(); i++)
+
+		for (int i = hideNeurons.size() - 1; i > -1; i--)
 		{
 			for (int j = 0; j < hideNeurons[i].size(); j++)
 			{
 				hideNeurons[i][j].setGradient(hideNeurons[i][j].getError() * derivative(hideNeurons[i][j].getValue()));
+			}
+		}
+		for (int i = hideNeurons.size() - 1; i > -1 ; i--)
+		{
+			for (int j = 0; j < hideNeurons[i].size(); j++)
+			{
+				if (i == hideNeurons.size() - 1)
+				{
+					for (int k = 0; k < outputNeurons.size(); k++)
+					{
+						hideNeurons[i][j].setWeights(k, hideNeurons[i][j].getWeights()[k] + learningRate * outputNeurons[k].getGradient() * hideNeurons[i][j].getValue());
+					}
+				}
+				else
+				{
+					for (int k = 0; k < hideNeurons[i + 1].size(); k++)
+					{
+						hideNeurons[i][j].setWeights(k, hideNeurons[i][j].getWeights()[k] + learningRate * hideNeurons[i + 1][k].getGradient() * hideNeurons[i][j].getValue());
+					}
+				}
 			}
 		}
 		for (int i = 0; i < inputNeurons.size(); i++)
@@ -237,17 +250,13 @@ public:
 				inputNeurons[i].setWeights(j, inputNeurons[i].getWeights()[j] + learningRate * hideNeurons[0][j].getGradient() * inputNeurons[i].getValue());
 			}
 		}
-		for (int j = 0; j < inputNeurons.size(); j++)
-		{
-			inputNeurons[j].setBias(inputNeurons[j].getBias() + learningRate * inputNeurons[j].getGradient());
-		}
 	}
 };
 int main()
 {
 	std::vector<std::vector<double>> inputSet = { {0, 0}, {0, 1}, {1, 0}, {1, 1} };
-	std::vector<double> outputSet = { 0, 1, 1, 0 };
-	NeuralNetwork nn(2, { 2, 2 }, 1, 1, 0.5, true);
+	std::vector<double> outputSet = { 1, 0, 0, 0 };
+	NeuralNetwork nn(2, { 2 }, 1, 5, 0.5, true);
 	int iteration = 10000;
 	for (int i = 0; i < iteration; i++)
 	{
